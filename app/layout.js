bacbf3cbfb851a9a781a61aacc7cc2ba8a864b71
Naitik Pawar <73638679+naitikpawar22@users.noreply.github.com>
2026-03-15 17:54:01 +0530

    Integrate Geist fonts and update RootLayout
    Added Geist font integration and applied classes to body.

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,15 @@ import "./globals.css";
 import Provider from "./provider";
 import ConvexClientProvider from "./ConvexClientProvider";
 
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -11,14 +20,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning >
-      <body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <ConvexClientProvider>
-        <Provider>
-        {children}
-        </Provider>
+          <Provider>
+            {children}
+          </Provider>
         </ConvexClientProvider>
-        
       </body>
     </html>
   );
